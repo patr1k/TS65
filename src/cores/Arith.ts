f2710b910bb13e363d0aea9c2fe970eb55992cf2
Patrik <pat.deloughry@gmail.com>
2024-09-18 10:17:16 -0600

    Fixed Negative flag bug

--- src/cores/Arith.ts
+++ src/cores/Arith.ts
@@ -12,7 +12,7 @@ export function adc_im(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -29,7 +29,7 @@ export function adc_zpg(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -46,7 +46,7 @@ export function adc_zpgx(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -63,7 +63,7 @@ export function adc_abs(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -80,7 +80,7 @@ export function adc_absx(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -97,7 +97,7 @@ export function adc_absy(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -115,7 +115,7 @@ export function adc_indx(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -133,7 +133,7 @@ export function adc_indy(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
     comp.cpu.reg.V = (result & 0x100) > 0;
 }
@@ -145,65 +145,72 @@ export function cmp_im(value: byte) {
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_zpg(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_zpgx(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.X);
+    const prev = comp.bus.readByte(value + comp.cpu.reg.X);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_abs(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_absx(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.X);
+    const prev = comp.bus.readByte(value + comp.cpu.reg.X);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_absy(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.Y);
+    const prev = comp.bus.readByte(value + comp.cpu.reg.Y);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_indx(value: byte) {
     const addr = comp.bus.readByte(value + comp.cpu.reg.X);
-    const result = comp.cpu.reg.A - comp.bus.readByte(addr);
+    const prev = comp.bus.readByte(addr);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cmp_indy(value: byte) {
     const addr = comp.bus.readByte(value) + comp.cpu.reg.X;
-    const result = comp.cpu.reg.A - comp.bus.readByte(addr);
+    const prev = comp.bus.readByte(addr);
+    const result = comp.cpu.reg.A - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 
@@ -213,23 +220,25 @@ export function cpx_im(value: byte) {
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cpx_zpg(value: byte) {
-    const result = comp.cpu.reg.X - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.X - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cpx_abs(value: byte) {
-    const result = comp.cpu.reg.X - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.X - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 
@@ -239,23 +248,25 @@ export function cpy_im(value: byte) {
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cpy_zpg(value: byte) {
-    const result = comp.cpu.reg.Y - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.Y - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 export function cpy_abs(value: byte) {
-    const result = comp.cpu.reg.Y - comp.bus.readByte(value);
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.Y - prev;
 
     comp.cpu.reg.C = result > 0;
     comp.cpu.reg.Z = result === 0;
-    comp.cpu.reg.N = (result & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
 }
 
 
@@ -265,52 +276,57 @@ export function sbc_im(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
 export function sbc_abs(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value) - (1 - (comp.cpu.reg.C ? 1 : 0));
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.A - prev - (1 - (comp.cpu.reg.C ? 1 : 0));
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
 export function sbc_absx(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.X) - (1 - (comp.cpu.reg.C ? 1 : 0));
+    const prev = comp.bus.readByte(value + comp.cpu.reg.X);
+    const result = comp.cpu.reg.A - prev - (1 - (comp.cpu.reg.C ? 1 : 0));
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
 export function sbc_absy(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.Y) - (1 - (comp.cpu.reg.C ? 1 : 0));
+    const prev = comp.bus.readByte(value + comp.cpu.reg.Y);
+    const result = comp.cpu.reg.A - prev - (1 - (comp.cpu.reg.C ? 1 : 0));
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
 export function sbc_zpg(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value) - (1 - (comp.cpu.reg.C ? 1 : 0));
+    const prev = comp.bus.readByte(value);
+    const result = comp.cpu.reg.A - prev - (1 - (comp.cpu.reg.C ? 1 : 0));
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
 export function sbc_zpgx(value: byte) {
-    const result = comp.cpu.reg.A - comp.bus.readByte(value + comp.cpu.reg.X) - (1 - (comp.cpu.reg.C ? 1 : 0));
+    const prev = comp.bus.readByte(value + comp.cpu.reg.X);
+    const result = comp.cpu.reg.A - prev - (1 - (comp.cpu.reg.C ? 1 : 0));
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((prev && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
@@ -321,7 +337,7 @@ export function sbc_indx(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
 
@@ -332,6 +348,6 @@ export function sbc_indy(value: byte) {
 
     comp.cpu.reg.A = result & 0xFF;
     comp.cpu.reg.Z = comp.cpu.reg.A === 0;
-    comp.cpu.reg.N = (comp.cpu.reg.A & 0x80) > 0;
+    comp.cpu.reg.N = ((value && 0x80) === 0) && ((result & 0x80) > 0);
     comp.cpu.reg.C = (result & 0x100) > 0;
 }
